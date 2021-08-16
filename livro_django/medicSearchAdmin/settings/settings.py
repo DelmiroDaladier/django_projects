9e761aa0cf888086c6f712e9778079a72ec54869
Your Name <you@example.com>
2021-08-15 17:29:57 -0700

    changes: medicSearch application

--- livro_django/medicSearchAdmin/settings/settings.py
+++ livro_django/medicSearchAdmin/settings/settings.py
@@ -28,13 +28,14 @@ DEBUG = True
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS	=	[
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'medicSearch'
 ]
 
 MIDDLEWARE = [
