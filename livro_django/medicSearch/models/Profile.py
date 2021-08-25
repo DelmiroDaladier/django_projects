1ac3d40dcda3519c65771c672fb9056ed2c6da1d
Your Name <you@example.com>
2021-08-24 21:29:20 -0300

    changes: add Profile model

--- livro_django/medicSearch/models/Profile.py
+++ livro_django/medicSearch/models/Profile.py
@@ -2,8 +2,8 @@ from medicSearch.models import *
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-	role = models.IntegerField(choices=ROLE_CHOICE,	default=3)
-	birthday = models.DateField(default=None,	null=True,	blank=True)
+    role = models.IntegerField(choices=ROLE_CHOICE,default=3)
+    birthday = models.DateField(default=None, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     token = models.CharField(max_length=255, null=True, blank=True)
@@ -12,16 +12,16 @@ class Profile(models.Model):
         return '{}'.format(self.user.username)
 
     @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
+    def create_user_profile(sender,instance,created,**kwargs):
         try:
             if created:
                 Profile.objects.create(user=instance)
         except:
             pass
 
-    @receiver(post_save,	sender=User)
-	def	save_user_profile(sender,	instance,	**kwargs):
-		try:
-			instance.profile.save()
-		except:
-			pass
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        try:
+            instance.profile.save()
+        except:
+            pass
