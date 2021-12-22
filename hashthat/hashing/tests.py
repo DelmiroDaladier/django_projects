1e7e32058d7dcdd6714da12a9e822d321204fd25
Your Name <you@example.com>
2021-12-22 20:32:56 -0300

    changes: add new tests

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,7 +1,9 @@
+from django.http import response
 from django.test import TestCase
 from selenium import webdriver
 from .forms import HashForm
 import hashlib
+from .models import Hash
 
 """ class FunctionalTestsCase(TestCase):
 
@@ -36,3 +38,20 @@ class UnitTestCase(TestCase):
         text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
 
+    def save_hash(self):
+        hash = Hash()
+        hash.text = 'hello'
+        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        hash.save()
+
+        return hash
+
+    def test_hash_object(self):
+        hash = self.save_hash()
+        pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertEqual(hash.text, pulled_hash.text)
+
+    def test_viewing_hash(self):
+        hash = self.save_hash()
+        response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertContains(response,'hello')
