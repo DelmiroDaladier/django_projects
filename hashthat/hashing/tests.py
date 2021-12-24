d4894121efd093eb42e5a80898a99ebb26df62d4
Your Name <you@example.com>
2021-12-24 17:05:38 -0300

    changes: add quickhash

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -5,6 +5,7 @@ from .forms import HashForm
 import hashlib
 from .models import Hash
 from django.core.exceptions import ValidationError
+import time
 
 class FunctionalTestsCase(TestCase):
 
@@ -20,6 +21,13 @@ class FunctionalTestsCase(TestCase):
         text = self.browser.find_element_by_id('id_text')
         text.send_keys('hello')
         self.browser.find_element_by_name('submit').click()
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+
+    def test_hash_ajax(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        time.sleep(5)
         self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
     def tearDown(self):
