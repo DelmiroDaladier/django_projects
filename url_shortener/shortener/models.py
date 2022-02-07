92d96474c331bc510f4c1f95b3bfcc5ccfa1b55e
Your Name <daladiersampaio@gmail.com>
2022-02-06 22:23:57 -0300

    Add:url shortener functionality

--- url_shortener/shortener/models.py
+++ url_shortener/shortener/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Urls(models.Model):
+class Url(models.Model):
     link = models.CharField(max_length=10000)
     uuid = models.CharField(max_length=10)
