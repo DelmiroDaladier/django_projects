9a7d801176b080478a7628da5d4e6284a25157eb
Your Name <daladiersampaio@gmail.com>
2022-02-23 21:42:37 -0300

    add: test_home_page_template

--- url_shortener/shortener/tests.py
+++ url_shortener/shortener/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class UnitTestCase(TestCase):
+
+    def test_home_page_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'shortener/index.html') 
