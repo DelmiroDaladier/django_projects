6da498265a7036379c863cb52d1f80c0c5c5c76e
Your Name <daladiersampaio@gmail.com>
2022-02-23 22:00:53 -0300

    Add: test_url_object

--- url_shortener/shortener/tests.py
+++ url_shortener/shortener/tests.py
@@ -1,4 +1,8 @@
+import uuid
+
+from .models import Url
 from django.test import TestCase
+from .view_helpers import create as create_helper
 
 # Create your tests here.
 class UnitTestCase(TestCase):
@@ -6,3 +10,19 @@ class UnitTestCase(TestCase):
     def test_home_page_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'shortener/index.html') 
+    
+    def save_url(self):
+        link = 'http://www.example.com'
+        link = create_helper.remove_http(link)
+   
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+
+        return new_url
+
+    def test_url_object(self):
+        url = self.save_url()
+        pulled_url = Url.objects.get(link='www.example.com')
+        self.assertEqual(url.link, pulled_url.link)
+
